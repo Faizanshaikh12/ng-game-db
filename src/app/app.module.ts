f8782167d29f83bb16c7c5cc1fe21c59b546f5b4
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-10-01 23:35:49 +0530

    intial commmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from './components/home/home.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
